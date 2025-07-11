0500fd88ab3e25b1ee719f66a6b074e71c9b8048
Leonardo Gigenas <lgigenas@gmail.com>
2025-07-11 11:34:29 -0300

    sasa

--- YouTubeDownloader/Form1.Designer.vb
+++ YouTubeDownloader/Form1.Designer.vb
@@ -22,14 +22,18 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         btnDescargar = New Button()
         txtUrl = New TextBox()
         txtLog = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        cmbFormato = New ComboBox()
         SuspendLayout()
         ' 
         ' btnDescargar
         ' 
-        btnDescargar.Location = New Point(12, 71)
+        btnDescargar.Location = New Point(12, 78)
         btnDescargar.Name = "btnDescargar"
         btnDescargar.Size = New Size(75, 23)
         btnDescargar.TabIndex = 0
@@ -39,7 +43,9 @@ Partial Class Form1
         ' txtUrl
         ' 
         txtUrl.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
-        txtUrl.Location = New Point(12, 31)
+        txtUrl.BackColor = Color.FromArgb(CByte(255), CByte(255), CByte(192))
+        txtUrl.ForeColor = Color.White
+        txtUrl.Location = New Point(12, 42)
         txtUrl.Name = "txtUrl"
         txtUrl.Size = New Size(429, 23)
         txtUrl.TabIndex = 1
@@ -47,23 +53,55 @@ Partial Class Form1
         ' txtLog
         ' 
         txtLog.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
-        txtLog.Location = New Point(12, 115)
+        txtLog.Location = New Point(12, 120)
         txtLog.Multiline = True
         txtLog.Name = "txtLog"
-        txtLog.Size = New Size(429, 128)
+        txtLog.ReadOnly = True
+        txtLog.Size = New Size(429, 178)
         txtLog.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 18)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(97, 15)
+        Label1.TabIndex = 3
+        Label1.Text = "URL de YouTube:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(124, 81)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(55, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "Formato:"
+        ' 
+        ' cmbFormato
+        ' 
+        cmbFormato.DropDownStyle = ComboBoxStyle.DropDownList
+        cmbFormato.FormattingEnabled = True
+        cmbFormato.Location = New Point(185, 78)
+        cmbFormato.Name = "cmbFormato"
+        cmbFormato.Size = New Size(121, 23)
+        cmbFormato.TabIndex = 5
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(453, 255)
+        ClientSize = New Size(453, 310)
+        Controls.Add(cmbFormato)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
         Controls.Add(txtLog)
         Controls.Add(txtUrl)
         Controls.Add(btnDescargar)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
         MinimumSize = New Size(469, 294)
         Name = "Form1"
-        Text = "YouTubeDownloader"
+        Text = "YouTube Downloader (by Leo)"
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -71,5 +109,8 @@ Partial Class Form1
     Friend WithEvents btnDescargar As Button
     Friend WithEvents txtUrl As TextBox
     Friend WithEvents txtLog As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbFormato As ComboBox
 
 End Class
--- YouTubeDownloader/Form1.vb
+++ YouTubeDownloader/Form1.vb
@@ -7,12 +7,17 @@ Public Class Form1
     Private outputFolder As String = Path.Combine(Application.StartupPath, "Descargas")
 
 
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        cmbFormato.Items.Add("Audio (MP3)")
+        cmbFormato.Items.Add("Video (MP4)")
+        cmbFormato.SelectedIndex = 0 ' Valor por defecto
 
 
         Try
-            ExtractEmbeddedResource("YouTubeDownloader.Resources.yt-dlp.exe", ytDlpPath)
-            ExtractEmbeddedResource("YouTubeDownloader.Resources.ffmpeg.exe", ffmpegPath)
+            ExtractEmbeddedResource("YouTubeDownloader.yt-dlp.exe", ytDlpPath)
+            ExtractEmbeddedResource("YouTubeDownloader.ffmpeg.exe", ffmpegPath)
         Catch ex As Exception
             MessageBox.Show("Error al extraer ejecutables: " & ex.Message)
             Application.Exit()
@@ -40,13 +45,29 @@ Public Class Form1
             Return
         End If
 
-        If Not Directory.Exists(outputFolder) Then
-            Directory.CreateDirectory(outputFolder)
-        End If
+        ' Abrir el selector de carpeta
+        Using fbd As New FolderBrowserDialog
+            fbd.Description = "Elegí la carpeta donde se guardarán los MP3"
+            If fbd.ShowDialog() <> DialogResult.OK Then
+                txtLog.AppendText("Descarga cancelada por el usuario." & vbCrLf)
+                Return
+            End If
+            outputFolder = fbd.SelectedPath
+            txtLog.AppendText("Carpeta seleccionada: " & outputFolder & vbCrLf)
+        End Using
 
+        btnDescargar.Enabled = False
         txtLog.AppendText("Iniciando descarga..." & vbCrLf)
 
-        Dim args As String = $"--extract-audio --audio-format mp3 --audio-quality 192K --ffmpeg-location ""{ffmpegPath}"" -o ""{outputFolder}\%(title)s.%(ext)s"" {url}"
+
+        Dim args As String = ""
+
+        If cmbFormato.SelectedItem.ToString() = "Audio (MP3)" Then
+            args = $"--extract-audio --audio-format mp3 --audio-quality 192K --ffmpeg-location ""{ffmpegPath}"" -o ""{Path.Combine(outputFolder, "%(title)s.%(ext)s")}"" {url}"
+        ElseIf cmbFormato.SelectedItem.ToString() = "Video (MP4)" Then
+            args = $"-f bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4 --merge-output-format mp4 --ffmpeg-location ""{ffmpegPath}"" -o ""{Path.Combine(outputFolder, "%(title)s.%(ext)s")}"" {url}"
+        End If
+
 
         Dim proc As New Process()
         proc.StartInfo.FileName = ytDlpPath
@@ -68,13 +89,23 @@ Public Class Form1
                                                End If
                                            End Sub
 
+        AddHandler proc.Exited, Sub()
+                                    Me.Invoke(Sub()
+                                                  btnDescargar.Enabled = True
+                                                  MessageBox.Show("Descarga finalizada.", "Completado", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                                              End Sub)
+                                End Sub
+
         Try
+            proc.EnableRaisingEvents = True
             proc.Start()
             proc.BeginOutputReadLine()
             proc.BeginErrorReadLine()
         Catch ex As Exception
             MessageBox.Show("Error al ejecutar la descarga: " & ex.Message)
+            btnDescargar.Enabled = True
         End Try
+
     End Sub
 
 
