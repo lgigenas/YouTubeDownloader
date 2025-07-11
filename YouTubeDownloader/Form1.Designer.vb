6a44f7ab7f2ee12792dd8690445f27695bd0915b
Leonardo Gigenas <lgigenas@gmail.com>
2025-07-11 09:06:25 -0300

    Agregar archivos de proyecto.

---
+++ YouTubeDownloader/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnDescargar = New Button()
+        txtUrl = New TextBox()
+        txtLog = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnDescargar
+        ' 
+        btnDescargar.Location = New Point(12, 71)
+        btnDescargar.Name = "btnDescargar"
+        btnDescargar.Size = New Size(75, 23)
+        btnDescargar.TabIndex = 0
+        btnDescargar.Text = "Descargar"
+        btnDescargar.UseVisualStyleBackColor = True
+        ' 
+        ' txtUrl
+        ' 
+        txtUrl.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        txtUrl.Location = New Point(12, 31)
+        txtUrl.Name = "txtUrl"
+        txtUrl.Size = New Size(429, 23)
+        txtUrl.TabIndex = 1
+        ' 
+        ' txtLog
+        ' 
+        txtLog.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        txtLog.Location = New Point(12, 115)
+        txtLog.Multiline = True
+        txtLog.Name = "txtLog"
+        txtLog.Size = New Size(429, 128)
+        txtLog.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(453, 255)
+        Controls.Add(txtLog)
+        Controls.Add(txtUrl)
+        Controls.Add(btnDescargar)
+        MinimumSize = New Size(469, 294)
+        Name = "Form1"
+        Text = "YouTubeDownloader"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnDescargar As Button
+    Friend WithEvents txtUrl As TextBox
+    Friend WithEvents txtLog As TextBox
+
+End Class
---
+++ YouTubeDownloader/Form1.vb
@@ -0,0 +1,82 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private ytDlpPath As String = Path.Combine(Path.GetTempPath(), "yt-dlp.exe")
+    Private ffmpegPath As String = Path.Combine(Path.GetTempPath(), "ffmpeg.exe")
+    Private outputFolder As String = Path.Combine(Application.StartupPath, "Descargas")
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        Try
+            ExtractEmbeddedResource("YouTubeDownloader.Resources.yt-dlp.exe", ytDlpPath)
+            ExtractEmbeddedResource("YouTubeDownloader.Resources.ffmpeg.exe", ffmpegPath)
+        Catch ex As Exception
+            MessageBox.Show("Error al extraer ejecutables: " & ex.Message)
+            Application.Exit()
+        End Try
+
+
+
+    End Sub
+
+    Private Sub ExtractEmbeddedResource(resourceName As String, outputPath As String)
+        Using stream = Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName)
+            If stream Is Nothing Then
+                Throw New Exception("No se encontró el recurso: " & resourceName)
+            End If
+            Using output = New FileStream(outputPath, FileMode.Create)
+                stream.CopyTo(output)
+            End Using
+        End Using
+    End Sub
+
+    Private Sub btnDescargar_Click(sender As Object, e As EventArgs) Handles btnDescargar.Click
+        Dim url As String = txtUrl.Text.Trim()
+        If String.IsNullOrWhiteSpace(url) Then
+            MessageBox.Show("Por favor ingresá una URL.")
+            Return
+        End If
+
+        If Not Directory.Exists(outputFolder) Then
+            Directory.CreateDirectory(outputFolder)
+        End If
+
+        txtLog.AppendText("Iniciando descarga..." & vbCrLf)
+
+        Dim args As String = $"--extract-audio --audio-format mp3 --audio-quality 192K --ffmpeg-location ""{ffmpegPath}"" -o ""{outputFolder}\%(title)s.%(ext)s"" {url}"
+
+        Dim proc As New Process()
+        proc.StartInfo.FileName = ytDlpPath
+        proc.StartInfo.Arguments = args
+        proc.StartInfo.UseShellExecute = False
+        proc.StartInfo.RedirectStandardOutput = True
+        proc.StartInfo.RedirectStandardError = True
+        proc.StartInfo.CreateNoWindow = True
+
+        AddHandler proc.OutputDataReceived, Sub(s, ev)
+                                                If ev.Data IsNot Nothing Then
+                                                    txtLog.Invoke(Sub() txtLog.AppendText(ev.Data & vbCrLf))
+                                                End If
+                                            End Sub
+
+        AddHandler proc.ErrorDataReceived, Sub(s, ev)
+                                               If ev.Data IsNot Nothing Then
+                                                   txtLog.Invoke(Sub() txtLog.AppendText("ERROR: " & ev.Data & vbCrLf))
+                                               End If
+                                           End Sub
+
+        Try
+            proc.Start()
+            proc.BeginOutputReadLine()
+            proc.BeginErrorReadLine()
+        Catch ex As Exception
+            MessageBox.Show("Error al ejecutar la descarga: " & ex.Message)
+        End Try
+    End Sub
+
+
+
+End Class
